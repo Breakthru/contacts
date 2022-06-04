3879c081559e853ebd5e9c242269f65eaf1a5007
Marco Paladini <paladini.marco@gmail.com>
2022-06-04 13:47:11 +0100

    use password_verify for login

--- app/index.php
+++ app/index.php
@@ -1,8 +1,4 @@
-<?php include_once("login.php");
-if (!($_SESSION['controllo_user']==$user AND $_SESSION['controllo_password']==$password))
-{
-die();
-}?>
+<?php include_once("login.php"); ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
@@ -33,7 +29,7 @@ while ($row = $result->fetch_assoc()) {
     echo "<div class='ticket'>";
     echo "<a href='index.php?b=".$row['id']."&admin=".$_GET['admin']."' class='button'>".$row['name']."</a>";
     echo "<br />";
-    if($_GET['admin']) {
+    if(isset($_GET['admin'])) {
         echo "<form action='editBusiness.php' method='POST'>";
         echo "<input type='hidden' name='businessIdToEdit' value='".$row['id']."'>";
         echo "<input class='button' type='submit' value='edit'></form>";
@@ -45,7 +41,7 @@ echo "</div>";
 
 function business_id_select($mysqli)
 {
-if($_GET['b']) {
+if(isset($_GET['b'])) {
     echo "<input type='hidden' name='business_id' value='".$_GET['b']."'>";
     return;
 }
@@ -67,7 +63,7 @@ echo "</select><br/>";
 function ticketList($mysqli)
 {
 // Perform Query
-if(!$_GET['b']) return;
+if(!isset($_GET['b'])) return;
 $result = $mysqli->query("select t.id as id, b.name as name, t.what as what, t.payment as payment, t.last_modified as date from brm_tickets t left join brm_business b on b.id = t.business_id where b.id =
 ".$mysqli->real_escape_string($_GET['b'])." order by t.last_modified desc;");
 // This shows the actual query sent to MySQL, and the error. Useful for debugging.
@@ -80,7 +76,7 @@ while ($row = $result->fetch_assoc()) {
     echo "<div class='ticket'>";
     echo "[".$row['id']."] ".$row['name'].": ".$row['what']."<br/>cost: ".$row['payment']."<br/>date: ".$row['date']." CET";
     echo "<br />\n";
-    if($_GET['admin']) {
+    if(isset($_GET['admin'])) {
         echo "<form action='editTicket.php' method='POST'>";
         echo "<input type='hidden' name='ticketIdToEdit' value='".$row['id']."'>";
         echo "<input class='button' type='submit' value='edit'></form>";
@@ -92,7 +88,7 @@ echo "</div>";
 
 function contacts($mysqli)
 {
-if(!$_GET['b']) return;
+if(!isset($_GET['b'])) return;
 // Perform Query
 $result = $mysqli->query("select c.id as id, b.name as bname, c.name as name, c.mail as mail, c.tel as tel from brm_contact c left join brm_business b on b.id = c.business_id
  where b.id = ".$mysqli->real_escape_string($_GET['b'])." order by c.last_modified desc;");
@@ -106,7 +102,7 @@ while ($row = $result->fetch_assoc()) {
     echo "<div class='ticket'>";
     echo $row['bname'].": ".$row['name']." , email: <a href = 'mailto:".$row['mail']."'>".$row['mail']."</a>, tel: ".$row['tel'];
     echo "<br />\n";
-    if($_GET['admin']) {
+    if(isset($_GET['admin'])) {
         echo "<form action='editContact.php' method='POST'>";
         echo "<input type='hidden' name='contactIdToEdit' value='".$row['id']."'>";
         echo "<input class='button' type='submit' value='edit'></form>";
@@ -119,14 +115,14 @@ echo "</div>";
 
 <div id="header">
 <a href="index.php" class="button">home</a>
-<?php if($_GET['b']) { ?>
+<?php if(isset($_GET['b'])) { ?>
 <a href="index.php?b=<?php echo $_GET['b'] ?>&admin=true" class="button">edit</a>
 <?php } else { ?>
 <a href="index.php?admin=true" class="button">edit</a>
 <?php } ?>
 </div>
 
-<?php if($_GET['b']) { ?>
+<?php if(isset($_GET['b'])) { ?>
 <div class='business_details'>
 <?php
 $result = $mysqli->query("select * from brm_business where id = ".$mysqli->real_escape_string($_GET['b']).";");
--- app/login.php
+++ app/login.php
@@ -1,47 +1,44 @@
 <?php
-if (!str_contains($_SERVER['HTTP_HOST'], "192.168") && $_SERVER['HTTP_HOST'] !== "localhost" && $_SERVER["HTTP_X_FORWARDED_PROTO"] !== "https") {
+// redirect to https
+if (!str_starts_with($_SERVER['HTTP_HOST'], "192.168") && $_SERVER['HTTP_HOST'] !== "localhost" && $_SERVER["HTTP_X_FORWARDED_PROTO"] !== "https") {
     $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' . $location);
     exit;
 }
 session_start();
+// read credentials from disk
 include_once("codes.php");
 
 function printLoginForm()
 {
-echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">";
-echo "<html>";
-echo "<head><title>contact manager application</title>";
-echo "<meta name='viewport' content='width=device-width, initial-scale=1' />";
-echo "</head><body>";
-echo "<div style='text-align: center;'>";
-echo "<form action='index.php' method='post'>";
-echo "<label for='user'>username&nbsp;</label>";
-echo "<input name='user' type='text' /><br/>";
-echo "<label for='password'>password&nbsp;</label>";
-echo "<input name='password' type='password' /><br/>";
-echo "<input type='submit' value='login' />";
-echo "</form>";
-echo "</body></html>";
-echo "</div>";
-die();
+    echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">";
+    echo "<html>";
+    echo "<head><title>contact manager application</title>";
+    echo "<meta name='viewport' content='width=device-width, initial-scale=1' />";
+    echo "</head><body>";
+    echo "<div style='text-align: center;'>";
+    echo "<form action='index.php' method='post'>";
+    echo "<label for='user'>username&nbsp;</label>";
+    echo "<input name='user' type='text' /><br/>";
+    echo "<label for='password'>password&nbsp;</label>";
+    echo "<input name='password' type='password' /><br/>";
+    echo "<input type='submit' value='login' />";
+    echo "</form>";
+    echo "</body></html>";
+    echo "</div>";
+    die();
 }
 
-if ($_POST['user']==$user AND $_POST['password']==$password )
-{	// Accesso consentito
-// il post e' valido
-	$_SESSION['controllo_user']=$_POST['user'];
-	$_SESSION['controllo_password']=$_POST['password'];
-//	$_SESSION['controllo_utente']=$_SERVER['PHP_AUTH_USER'];
-//	$_SESSION['controllo_password']=$_SERVER['PHP_AUTH_PW'];
+if ($_POST['user']===$user AND password_verify($_POST['password'], $password_hash) )
+{	// login successful
+	$_SESSION['user_token']=$user_token;
 }
-if ($_SESSION['controllo_user']==$user AND $_SESSION['controllo_password']==$password)
+// check current session
+if (!array_key_exists('user_token', $_SESSION) || !$_SESSION['user_token']==$user_token)
 {
+    printLoginForm();
+    die();
 }
-else
-{
-printLoginForm();
-die();
-}
+// logged in.
 ?>
