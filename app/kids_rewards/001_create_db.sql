2876fa9b8aa7ed732acf8e975bb20bc23c10f65c
Marco Paladini <paladini.marco@gmail.com>
2022-08-14 14:08:04 +0100

    fix db

--- app/kids_rewards/001_create_db.sql
+++ app/kids_rewards/001_create_db.sql
@@ -1,8 +1,9 @@
-CREATE TABLE `toy_bank_transactions` (
-  `id` int NOT NULL,
+CREATE TABLE IF NOT EXISTS `toy_bank_transactions` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `account_from` varchar(255) NOT NULL,
   `account_to` varchar(255) NOT NULL,
   `amount` int NOT NULL,
   `reference` varchar(1024) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
